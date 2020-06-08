f5c389caa5d968ec1772cf796a1deb9f077e4720
Liam <monteroliam@gmail.com>
2020-06-08 12:37:58 +0100

    favourites route done. Commit being done before further work

--- backend/server/routes.py
+++ backend/server/routes.py
@@ -22,71 +22,86 @@ def getMovie(movieID):
     else:
         return 'something went wrong'
 
-#route for getting all favourites
+
+
+#route for getting all favourites  NEEDS FIXING
 @entertain.route('/favourites')
 def getAllFavourites():
-    fav = db.session.query(favourite_movies).all() #.join(Users)
-    fav = json.dumps(fav)
+    fav = favourite_movies.query.all()
+
     if fav:
-        return fav
+        return fav[0].mood
     else:
         return 'something went wrong'
 
-# @entertain.route('/addmovie/<userID>') #,methods=['POST']
-# def postMovie(userID):
-#     movie = Movies(title='test title',description='test description')
-#     db.session.add(movie)
-
-#     statement = favourite_movies.insert().values(user_id=2, movie_id=1, mood='sad')
-#     db.session.execute(statement)
-
-#     db.session.commit()
-
-#     return "adding movie"
-
-@entertain.route('/users') #broken
-def getUsers():
+@entertain.route('/favourites/<userID>')
+def getUserFavourites(userID):
     
-    query = db.session.query(Users).all()
-    users = []
+    titles = []
+    results = []
+    userFavourites = db.session.query(Movies, favourite_movies).join(favourite_movies, (favourite_movies.movie_id == Movies.movie_id)).filter_by(user_id = userID).all()
+    # print(json.dumps([dict(r) for r in userFavourites]))
+    for fav in userFavourites:
+        results.append(fav.Movies.title)
+        # print(fav.reg_date)
+    
+    return json.dumps(results)
 
-    if query:
-        for u in query:
-            users.append(u.__dict__)
-            print(users)
-            return users
-    else:
-        return 'something went wrong'
+@entertain.route('/addfavourites/<userID>/<movieID>/<mood>') #WILL CHANGE TO POST LATER
+def postUserFavourites(userID, movieID, mood):
+    favourite = favourite_movies(user_id=userID, movie_id=movieID, mood=mood)
+
+    db.session.add(favourite)
+    db.session.commit()
+
+    # movie = Movies()
+
+    return "added"
+
+
+# @entertain.route('/users') #broken
+# def getUsers():
+    
+#     query = db.session.query(Users).all()
+#     users = []
+
+#     if query:
+#         for u in query:
+#             users.append(u.__dict__)
+#             print(users)
+#             return users
+#     else:
+#         return 'something went wrong'
 
 
 #route for getting user by id
-@entertain.route('/user/<userID>') # Returns a single user defined by its ID
-def getUser(userID):
-    user = Users.query.filter_by(user_id=userID).first()
-    if user:
-        return user.username
-    else:
-        return 'something went wrong'
+# @entertain.route('/user/<userID>') # Returns a single user defined by its ID
+# def getUser(userID):
+#     user = Users.query.filter_by(user_id=userID).first()
+#     if user:
+#         return user.username
+#     else:
+#         return 'something went wrong'
 
 
-@entertain.route('/user')
-def addUser():
-    user = Users(username="test username", email="google.com")
-    db.session.add(user)
-    db.session.commit()
-    return "adding user"
+# @entertain.route('/user')
+# def addUser():
+#     user = Users(username="test username", email="google.com")
+#     db.session.add(user)
+#     db.session.commit()
+#     return "adding user"
 
-@entertain.route('/user/<userID>', methods = ['DELETE'])
-def deleteUser(userID):
-    user = Users.query.filter_by(user_id=userID).first()
-    db.session.delete(user)
-    db.session.commit()
-    return "deleting user"
+# @entertain.route('/user/<userID>', methods = ['DELETE'])
+# def deleteUser(userID):
+#     user = Users.query.filter_by(user_id=userID).first()
+#     db.session.delete(user)
+#     db.session.commit()
+#     return "deleting user"
 
 
 
 #route for deleting from favourites table
-@entertain.route('/favourites/<userID>/<movieID>') #methods = ['DELETE'] )
+# @entertain.route('/favourites/<userID>/<movieID>') #methods = ['DELETE'] )   - uncomment to make work
 def deleteFavourites(userID, movieID):
     fav = favourite_movies.query.filter_by(user_id = userID, movie_id = movieID).one()
 
--- frontend/src/Containers/Home.js
+++ frontend/src/Containers/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 class HomePage extends Component {
     render() {
         <div>
+            <Button variant="primary" type="submit" id="login-btn" className="button"/>
             
         </div>
     }
