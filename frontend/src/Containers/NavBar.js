d808f79f3bfc20da79b31708da393322914c2063
matt6670 <malder103@gmail.com>
2020-06-10 16:18:57 +0100

    Delete account hides when logged out

--- frontend/src/Containers/NavBar.js
+++ frontend/src/Containers/NavBar.js
@@ -4,8 +4,8 @@ import {Navbar, Nav} from 'react-bootstrap';
 import '../css/NavBar.css';
 
 class NavBar extends React.Component {
-    a = <Navbar.Text><a href="http://localhost:5000/login" >Sign In or Register</a></Navbar.Text>;
-    b = <Navbar.Text><a onClick={this.props.resetId} href="http://localhost:5000/logout">Log Out</a></Navbar.Text>;
+    a = <Navbar.Text><a href="http://localhost:5000/login" >Sign In or Register</a></Navbar.Text>
+    b = <div><Navbar.Text><a onClick={this.props.resetId} href="http://localhost:5000/logout">Log Out</a></Navbar.Text><Button onClick={this.props.deleteMyAccount} >Delete My Account</Button></div>;
     render() {
         return(
             <div>
@@ -14,7 +14,7 @@ class NavBar extends React.Component {
                         Entertain.me
                     </Navbar.Brand>
                     {this.props.userId ? this.b : this.a}
-                    <Button onClick={this.props.deleteMyAccount} >Delete My Account</Button>     
+                         
                 </Navbar>
             </div>
         )
