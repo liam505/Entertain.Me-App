967ef141d68353b139ac108cd52329482b3584e4
Liam <monteroliam@gmail.com>
2020-06-11 18:47:09 +0100

    content for pre-login homepage

--- frontend/src/Containers/HomePage.js
+++ frontend/src/Containers/HomePage.js
@@ -80,14 +80,30 @@ class Homepage extends React.Component {
                 console.log(this.state.movieData)
                 console.log(" NO MOVIE DATA")
                 return (
-                    <div>
+                    <div className="d-flex flex-column h-70 align-items-center justify-content-center">
                         <SearchBar userId={this.props.userId} forceRender={this.forceRender} goBack={this.goBack} movieDataExists={this.movieDataExists}/>
                         {this.props.userId ? 
                             <div>
                                 
                                 <FavouriteMovies userId={this.props.userId}/>
-                            </div>
-                            : null}
+                            </div>: 
+                            <div className="h-100 w-100 d-flex flex-column bg-light align-items-center justify-content-between ">
+                                <p className="mb-0 display-4">Search for your favourites films</p>
+                                <div className="container w-50 d-flex flex-row align-items-center justify-content-around ">
+                                    <img style={{ height:'200px', width:'133px'}} src="https://www.bestmovieposters.co.uk/wp-content/uploads/2019/01/4A8ltk8G.jpeg" />
+                                    <img style={{ height:'200px', width:'133px'}} src="https://www.mauvais-genres.com/26247/avengers-endgame-original-movie-poster-15x21-in-2019-anthony-russo-robert-downey-jr.jpg" />
+                                    <img style={{ height:'200px', width:'133px'}} src="https://images-na.ssl-images-amazon.com/images/I/41a0nad8wPL._AC_.jpg" />
+                                </div>
+                                <p className="display-3">↓</p>
+                                <p className="display-4">Add them to mood-specific lists</p>
+                                <div className="container w-50 d-flex flex-row align-items-center justify-content-around">
+                                    <p className="display-1">😀</p>
+                                    <p className="display-1">😥</p>
+                                    <p className="display-1">🥱</p>
+                                </div>
+                                <p className="display-3">↓</p>
+                                <p className="display-4">Get mood-matched personalised recommendations!</p>
+                            </div>}
                     </div>
 
                 )
